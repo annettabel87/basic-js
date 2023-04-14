9d8b8c56dee1ffb3370752be3ec3740bae5026fe
Annettabel <annettabel@rambler.ru>
2023-04-14 16:30:39 +0500

    feat: add solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+  if (typeof (sampleActivity) !== 'string' || +sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY || isNaN(parseFloat(sampleActivity))) {
+    return false
+  } else {
+    let n = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity));
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    let result = Math.ceil(n / k);
+    if (result > 0 && result != Infinity) {
+      return (result)
+    } else {
+      return false
+    }
+  }
 }
 
 module.exports = {
