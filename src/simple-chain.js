5eb7c21eb1b9533fa52ae38f6d7697bbecc0cf49
Annettabel <annettabel@rambler.ru>
2023-04-14 18:04:05 +0500

    feat: add solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value !== undefined) {
+      this.chain.push(value)
+      return this
+    } else {
+      this.chain.push('( )')
+      return this
+    }
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position - 1 >= 0 && Number.isInteger(position - 1) && position  < this.chain.length ) {
+      this.chain.splice(position - 1, 1)
+      return this
+    } else {
+      this.chain = []
+      throw new Error('You can\'t remove incorrect link!')
+    }
+
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.chain.map(item => `( ${item} )`)
+    this.chain = []
+    return result.join('~~')
   }
 };
 
