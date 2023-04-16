cbd4f004d434f4a79cd230cbbcf45d150c538b2c
Annettabel <annettabel@rambler.ru>
2023-04-16 17:20:56 +0500

    feat: add solution dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getDNSStats( domains ) {
+  return domains.reduce((result, domain) => {
+		let domainStr = "";
+		const reverseArray =  domain.split(".").reverse()
 
+		for (arr of reverseArray) {
+			domainStr += `.${arr}`
+			if (!result[domainStr]) result[domainStr] = 0
+			result[domainStr]++
+		}
+
+		return result
+	}, {})
+}
+getDNSStats([
+    'code.yandex.ru',
+    'music.yandex.ru',
+    'yandex.ru'
+   ])
 module.exports = {
   getDNSStats
 };
